4a32bd3d47ec8da8d571c60e62b6c361e2d3297a
PradCoder <2010pesara@gmail.com>
2018-07-16 00:11:53 -0400

    Update sentAnyl.py

--- Python/sentAnyl.py
+++ Python/sentAnyl.py
@@ -1,11 +1,11 @@
 import tweepy
 from textblob import TextBlob
 
-consumer_key = 'vw7FNaJVb9fclrSjQDpgbMRfu'
-consumer_secret = 'yYbDzP4u4LbUNQxwXenlpp95HPq4ietHxXRamtovqev9xochFT'
+consumer_key = #filler
+consumer_secret = #filler
 
-access_token = "962193561121824769-KTGVVSCyZwz1ikrL5vjZOANDHKRCawA"
-access_token_secret = "q6oFScgrC6aNbFbPpb1ltl7kiPxxNEN8Ta58IFrGMRZoy"
+access_token = #filler
+access_token_secret = #filler
 
 auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
